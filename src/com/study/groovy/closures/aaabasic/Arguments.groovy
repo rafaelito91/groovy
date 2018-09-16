74b42577b2f0f78533c10b8f6c03d54be8a16e0b
Rafael Ito <rafaelito91@gmail.com>
2018-09-16 20:18:35 -0300

    basic concepts

--- src/com/study/groovy/closures/aaabasic/Arguments.groovy
+++ src/com/study/groovy/closures/aaabasic/Arguments.groovy
@@ -2,17 +2,7 @@ package com.study.groovy.closures.aaabasic
 
 class Arguments {
 
-    // if it was a script, no static was needed.
-    // it is so right know due to main method having
-    // static scope
-    static def timesTen(num, abacaxi) {
-        abacaxi(num * 10)
-    }
-
     static void main(String[] args) {
-        /*
-        1) Clojure arguments
-         */
         // Simple arugment definition: "*nameOfArgumet* ->"
         [1,2,3].each{ num ->
             print num
@@ -22,24 +12,5 @@ class Arguments {
         [1,2,3].each{
             print it
         }
-
-        timesTen(10, { println it })
-
-        // Groovy tells me that every time a closure is
-        // the last argument in a method, we can remove
-        // the method braces and write method as below:
-        timesTen(2) {
-            println it
-        }
-
-        // same reason we can write each method like:
-        [1,2,3].each{
-            print it
-        }
-        // instead of
-        [1,2,3].each({
-            print it
-        })
-        // because the closure is the last argument!!
     }
 }
--- src/com/study/groovy/closures/aaabasic/Definition.groovy
+++ src/com/study/groovy/closures/aaabasic/Definition.groovy
@@ -2,7 +2,31 @@ package com.study.groovy.closures.aaabasic
 
 class Definition {
 
+    // if it was a script, no static was needed.
+    // it is so right know due to main method having
+    // static scope
+    static def timesTen(num, abacaxi) {
+        abacaxi(num * 10)
+    }
+
     static void main(String[] args) {
-        
+        timesTen(10, { println it })
+
+        // Groovy tells me that every time a closure is
+        // the last argument in a method, we can remove
+        // the method braces and write method as below:
+        timesTen(2) {
+            println it
+        }
+
+        // same reason we can write each method like:
+        [1,2,3].each{
+            print it
+        }
+        // instead of
+        [1,2,3].each({
+            print it
+        })
+        // because the closure is the last argument!!
     }
 }
